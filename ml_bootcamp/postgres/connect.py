c4aa2b6bde6e275dda9e5b1e394898f43fd5a9e1
Gurubrahmanandam E <guru.prince8@gmail.com>
2024-10-29 18:43:00 -0500

    added count logic

--- ml_bootcamp/postgres/connect.py
+++ ml_bootcamp/postgres/connect.py
@@ -2,17 +2,19 @@ import os
 import psycopg2
 from config import load_config
 
-def connect(configuration):
+def connect(configuration,query:str):
     try:
         with psycopg2.connect(**configuration) as conn:
             print('connect to sql')
             with conn.cursor() as cur:
-                cur.execute("select * from ml.vendors")
+                cur.execute(query)
+                print(f'Total number of records fetched are {0}',cur.rowcount)
                 row = cur.fetchone()
                 while row is not None:
                     print(row)
                     row = cur.fetchone()
     except (psycopg2.DatabaseError, Exception) as err:
         print("Error is ",err)
-
-connect(load_config())
+query = "select * from ml.vendors"
+query = "select * from rules_241.pr_engineclasses"
+connect(load_config(),query)
