054e774a6ae5d90819b8bfca3f0a49134cf90202
Gurubrahmanandam E <guru.prince8@gmail.com>
2024-10-31 23:03:51 -0500

    added environment end point

--- web/server.py
+++ web/server.py
@@ -8,12 +8,26 @@ app = Flask(__name__)
 query = "select pzjar, pzpackage, pzclass from rules_241.pr_engineclasses"
 
 #  export DYLD_LIBRARY_PATH=/Library/PostgreSQL/16/lib
+environment = {
+    'k8s': 'flask-dev'
+}
+
+"""
+    fetches environment configurations
+"""
+
+
+@app.route("/environment")
+def fetch_environment():
+    return jsonify(environment)
+
 
 @app.route("/")
 def hell_world():
     pr1 = PR_ENGINE_CLASSES()
     print(pr1.count)
-    print("request received from {0} at {1} and headers are {2}".format(request.remote_addr, datetime.now(), request.headers))
+    print("request received from {0} at {1} and headers are {2}".format(request.remote_addr, datetime.now(),
+                                                                        request.headers))
     result = {"incomes": [
         {
             'description': 'Salary',
