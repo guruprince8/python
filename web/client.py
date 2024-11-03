135186e591f897df88fb3ea433a7f3273e3c5341
Gurubrahmanandam E <guru.prince8@gmail.com>
2024-11-02 21:45:06 -0500

    added client.py to explore rest client

---
+++ web/client.py
@@ -0,0 +1,18 @@
+import nltk
+import requests
+from bs4 import BeautifulSoup
+import tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+api_url = "https://en.wikipedia.org/wiki/Cloudflare"
+response = requests.get(api_url)
+soup = BeautifulSoup(response.text)
+# tokens = tokenize.generate_tokens(soup.get_text())
+# print(response.text)
+# print(soup.get_text())
+text = "Natural language processing (NLP) is a field of computer science, artificial intelligence and computational linguistics concerned with the interactions between computers and human (natural) languages, and, in particular, concerned with programming computers to fruitfully process large natural language corpora. Challenges in natural language processing frequently involve natural language understanding, natural language generation (frequently from formal, machine-readable logical forms), connecting language and machine perception, managing human-computer dialog systems, or some combination thereof."
+print(sent_tokenize(text))
+print(word_tokenize(text))
